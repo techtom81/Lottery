6dd1505cfcf5639113f145fbd8312ab505d22d69
Tom <tom.hunt@damdigital.com>
2018-03-20 14:24:47 +0000

    Added loops to compare picked numbers to drawn numbers

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -25,18 +25,44 @@ namespace Lottery
             Console.WriteLine("Please enter sixth number");
             int sixthNum = Convert.ToInt32(Console.ReadLine());
 
-            int[] numbersArr = { firstNum, secondNum, thirdNum, fourthNum, fifthNum, sixthNum };
-            string numbersStr = string.Join(",", numbersArr);
+            int[] pickedNumbers = { firstNum, secondNum, thirdNum, fourthNum, fifthNum, sixthNum };
+            string numbersStr = string.Join(",", pickedNumbers);
             Console.WriteLine("Thank you, your lottery numbers are {0}", numbersStr);
             Console.WriteLine("Press any key to start the draw, Good luck!");
             Console.ReadLine();
             int[] drawnNumbers = Draw();
             List<string> drawnStr = new List<string>();
+            List<string> matchedNums = new List<string>();
             foreach (int number in drawnNumbers)
             {
                 drawnStr.Add(number.ToString());
             }
             Console.WriteLine("The lottery numbers are {0}", string.Join(" ", drawnStr));
+
+            // compare pickedNumbers to drawnNumbers
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (pickedNumbers[i] == drawnNumbers[j])
+                    {
+                        matchedNums.Add(pickedNumbers[i].ToString());
+                    }
+                }
+            }
+            if (matchedNums.Count > 0 && matchedNums.Count < 6)
+            {
+                Console.WriteLine("Congratulations! you matched {0} numbers", matchedNums.Count);
+                Console.WriteLine("Your matched numbers are {0}", string.Join(" ", matchedNums));
+            }
+            else if (matchedNums.Count == 6)
+            {
+                Console.WriteLine("Congratulations you won the jackpot! You matched all 6 numbers");
+            }
+            else
+            {
+                Console.WriteLine("Unlucky! you didn't match any numbers. Try again");
+            }
         }
 
         private static int RandomNumber()
